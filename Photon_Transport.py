6dc845445aa5a415e2981931c276a8f93c555f1c
BenVoller <benvoller8@gmail.com>
2023-01-30 10:28:32 +0000

    Update Photon_Transport.py

--- Photon_Transport.py
+++ Photon_Transport.py
@@ -1,16 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import multiprocessing as mp
+import time
 
 
 np.random.seed(1234)
 
-class photon():
+class photons():
 
-    def init(self):
+    def __init__(self):
         # Defines the initial x,y,z coordinates to be 000 an the cosine 
-        self.init_pos = [0,0,0]
-        self.init_vel = [0,0,1]
+        self.pos = [0,0,0]
+        self.vel = [0,0,1]
 
         # Psuedo Random Number for the step size of the photon movement
         eta = np.random.random
@@ -41,31 +42,51 @@ class photon():
         pass
     
 
-def run():
-    # Runs the photon trasnport for Monte Carlo photon trasnport 
+def run(numberPhotons):
     
-    # Define Photon
+    
+        # Runs the photon trasnport for Monte Carlo photon trasnport 
+        
+        # Define Photon
+        photon = photons()
+        print (photon.pos)
+        # Set step size of photon according to -ln(eta) where eta is a psuedo random number
+
+        # Find Boundary distance or change in medium
+
+        # If step > distance to boundary d_b * u_t move to boundary and test for reflect or trasmit otherwise move.
+
+        # If not at boundary - Transmit, Absorb or Scatter
+
+        # photon dead test, i,e absorbed or out of bounds 
+
+        # weight check 
+
+        # Roulette 
+
+        # Repeat if photon is still alive
+
+        # Last Photon? Then End.
+
+    
 
 
-    # Set step size of photon according to -ln(eta) where eta is a psuedo random number
+if __name__ == '__main__':
+    t0 = time.time()
+    results = [for i in range(1000):
+        run(0)]
+    t2 = time.time()
+    '''
+    n_cpu = mp.cpu_count()  # = 8 
+    numberPhotons = 100000 # Number of photons
 
-    # Find Boundary distance or change in medium
+    pool = mp.Pool(processes=n_cpu) 
+    results = [pool.map(run, range(numberPhotons))]
 
-    # If step > distance to boundary d_b * u_t move to boundary and test for reflect or trasmit otherwise move.
-
-    # If not at boundary - Transmit, Absorb or Scatter
-
-    # photon dead test, i,e absorbed or out of bounds 
-
-    # weight check 
-
-    # Roulette 
-
-    # Repeat if photon is still alive
-
-    # Last Photon? Then End.
-
-    print ('Run')
+    t1 = time.time()
+    
+    print ('parallel time: ', t1 - t0)
+    '''
+    print ('Series Time: ', t2 - t0)
 
 
-print(f"Number of cpu: {mp.cpu_count()}")
