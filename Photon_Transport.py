ec6e1b8ab44d79c523c305476eaebe5b4f4b32ba
BenVoller <benvoller8@gmail.com>
2023-01-31 10:11:38 +0000

    Update Photon_Transport.py
    Input Fresnel Reflection into  the algorithm

--- Photon_Transport.py
+++ Photon_Transport.py
@@ -8,13 +8,25 @@ np.random.seed(1234)
 
 class photons():
 
-    def __init__(self):
+    def __init__(self, weight):
         # Defines the initial x,y,z coordinates to be 000 an the cosine 
         self.pos = [0,0,0]
         self.vel = [0,0,1]
 
+        self.W = weight 
+
         # Psuedo Random Number for the step size of the photon movement
         eta = np.random.random
+
+    def fresnelReflection(self, n0, n1):
+        
+        # Takes the two refractive indices of the ambient medium and the first medium and conmputes the 
+        # proportion that is refelcted according to Specualar Fresnel Reflectance 
+
+        Rsp = ((n0 - n1) / (n0 + n1))**2
+
+        self.W += -Rsp
+
     def stepSize(self):
         pass
 
@@ -40,16 +52,33 @@ class photons():
 
     def photon_dead(self):
         pass
-    
 
-def run(numberPhotons):
+class mediums():
+
+    def __init__(self, refractiveIndex):
+
+        # For now this is a homogeneous medium 
+        self.n0 = 1
+        self.n1 = refractiveIndex
+
+        
+        self.grid = np.zeros([1000,1000,1000])
+
+
+
+def run(medium):
     
     
         # Runs the photon trasnport for Monte Carlo photon trasnport 
         
         # Define Photon
-        photon = photons()
+        photon = photons(weight=1)
         print (photon.pos)
+        print (photon.W)
+        #print (photon.pos)
+
+        photon.fresnelReflection(medium.n0, medium.n1)
+        print(photon.W)
         # Set step size of photon according to -ln(eta) where eta is a psuedo random number
 
         # Find Boundary distance or change in medium
@@ -73,12 +102,17 @@ def run(numberPhotons):
 
 if __name__ == '__main__':
     t0 = time.time()
-    
-    n_cpu = mp.cpu_count()  # = 8 
-    numberPhotons = 100000 # Number of photons
 
-    pool = mp.Pool(processes=n_cpu) 
-    results = [pool.map(run, range(numberPhotons))]
+    n_cpu = mp.cpu_count()  # = 8 
+    numberPhotons = 1000 # Number of photons
+
+    medium1 = mediums(refractiveIndex=2)
+
+    #pool = mp.Pool(processes=n_cpu) 
+    #results = [pool.map(run, range(numberPhotons , medium1))]
+
+    run(medium1)
+
 
     t1 = time.time()
     
