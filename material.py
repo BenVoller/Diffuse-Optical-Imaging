08fabc3188d2e6db8eb7bfd750ff54da08093b93
BenVoller <benvoller8@gmail.com>
2023-03-13 15:47:33 +0000

    Update material.py

--- material.py
+++ material.py
@@ -2,18 +2,34 @@ import numpy as np
 
 class medium():
 
-    def __init__(self, n0=1, z0=2, n1=2, z1=4):
+    def __init__(self, n0=2, z1=2, n1=4, z2=4):
 
-        self.size = z0 + z1
+        self.size = z1 + z2
         # refractive indexes
-        self.z0 = z0
+        self.z0 = 0 
         self.z1 = z1
+        self.z2 = z2
+ 
         self.n0 = n0
         self.n1 = n1
+        
+   
+
+
+        self.layers = {0: self.n0,
+                       1: self.n1}
+        
+        self.distances = {0: 0,
+                       1: self.z1,
+                       2:self.z2,}
+
 
         self.z_array = np.ones(1000*self.size)
 
-      
+    
+    
+       
+            
 
     
 
