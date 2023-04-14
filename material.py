ca7b5ac1a56acf6acddc02f2250ba6344c835c1c
BenVoller <benvoller8@gmail.com>
2023-04-14 15:06:11 +0100

    Adding SORS

--- material.py
+++ material.py
@@ -2,28 +2,24 @@ import numpy as np
 
 class medium():
 
-    def __init__(self, n1=1, z1=0.01, n2=1, z2=0.02):
+    def __init__(self):
 
-        self.size = z1 + z2
+        
         # refractive indexes
-        self.z0 = 0 
-        self.z1 = z1
-        self.z2 = z2
-        
-        self.n0 = 1
-        self.n1 = n1
-        self.n2 = n2
+     
 
+         # [depth, refractive_index(n), u_a, u_t, g]
+        layer_null = [-999.9, 1, 1, 1, 0]
+        layer0 = [float(0), 1, 1, 1, 0]
+        layer1 = [0.01, 1, 10, 90, 0.75]
+        layer2 = [0.02, 1, 10, 90, 0.75]
+        layer3 = [999.9, 1, 1, 1, 0]
 
-        self.layers = {0: self.n0,
-                       1: self.n1,
-                       2:self.n2,
-                       3: self.n0}
-        
-        self.distances = {0: self.z0,
-                       1: self.z1,
-                       2:self.z2,
-                       3:99999}
+        self.layers = {-1:layer_null,
+                       0:layer0,
+                       1:layer1,
+                       2:layer2,
+                       3:layer3}
 
 
      
